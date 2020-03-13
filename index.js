e423d2f27ac2e01e5322a5b2d75ec667a48b989f
Josh schaben <jschaben42@gmail.com>
2020-03-13 17:37:12 -0500

    updated index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const postsRouter = require("./posts/posts-router.js");
+const postsRouter = require("./posts/post-router");
 const server = express();
 const cors = require("cors");
 
@@ -8,10 +8,10 @@ server.use(cors());
 server.use("/api/posts", postsRouter);
 
 server.get("/", (req, res) => {
-  res.send(`<h2>It's working It's working</h2>`);
+  res.send(`<h2>It's working</h2>`);
 });
 
-const port = 6000;
+const port = 5000;
 server.listen(port, () => {
   console.log(
     `Server Running on http://localhost:${port}`
