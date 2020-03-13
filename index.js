67e7fd2c48b26fefcdfb23d80c883d55da3099b2
Josh schaben <jschaben42@gmail.com>
2020-03-13 17:28:44 -0500

    set up th router for post

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const postsRouter = require("./posts/posts-router.js");
+const server = express();
+const cors = require("cors");
+
+server.use(express.json());
+server.use(cors());
+server.use("/api/posts", postsRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>It's working It's working</h2>`);
+});
+
+const port = 6000;
+server.listen(port, () => {
+  console.log(
+    `Server Running on http://localhost:${port}`
+  );
+});
